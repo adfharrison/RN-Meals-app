a6de178ca339e2bef575733785e18afe50a368b2
adfharrsion <68917507+adfharrsion@users.noreply.github.com>
2021-02-18 17:21:31 +0000

    add favorite star

--- screens/mealDetailsScreen.js
+++ screens/mealDetailsScreen.js
@@ -1,21 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { createStackNavigator } from 'react-navigation-stack';
-import { createBottomTabNavigator } from 'react-navigation-tabs';
-import { createDrawerNavigator } from 'react-navigation-drawer';
-import { MEALS } from '../data/dummyData';
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
-const MealDetailsScreen = (props) => {
+import { MEALS } from '../data/dummyData';
+import CustomHeaderButton from '../components/headerButton';
+
+const MealDetailScreen = (props) => {
   const mealId = props.navigation.getParam('mealId');
-  const selectedMeal = MEALS.find((meal) => {
-    return meal.id === mealId;
-  });
+
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
   return (
-    <View style={styles.container}>
+    <View style={styles.screen}>
       <Text>{selectedMeal.title}</Text>
       <Button
-        title='Go back to categories'
+        title='Go Back to Categories'
         onPress={() => {
           props.navigation.popToTop();
         }}
@@ -24,22 +23,86 @@ const MealDetailsScreen = (props) => {
   );
 };
 
-MealDetailsScreen.navigationOptions = (navigationData) => {
+MealDetailScreen.navigationOptions = (navigationData) => {
   const mealId = navigationData.navigation.getParam('mealId');
-  const selectedMeal = MEALS.find((meal) => {
-    return meal.id === mealId;
-  });
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
   return {
     headerTitle: selectedMeal.title,
+    headerRight: (
+      <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+        <Item
+          title='Favorite'
+          iconName='ios-star'
+          onPress={() => {
+            console.log('Mark as favorite!');
+          }}
+        />
+      </HeaderButtons>
+    ),
   };
 };
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
-export default MealDetailsScreen;
+export default MealDetailScreen;
+
+// import React, { useState, useEffect } from 'react';
+// import { StyleSheet, Text, View, Button } from 'react-native';
+// import { createStackNavigator } from 'react-navigation-stack';
+// import { createBottomTabNavigator } from 'react-navigation-tabs';
+// import { createDrawerNavigator } from 'react-navigation-drawer';
+// import { MEALS } from '../data/dummyData';
+// import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+// import { CustomHeaderButton } from '../components/headerButton';
+
+// const MealDetailsScreen = (props) => {
+//   const mealId = props.navigation.getParam('mealId');
+//   const selectedMeal = MEALS.find((meal) => {
+//     return meal.id === mealId;
+//   });
+
+//   return (
+//     <View style={styles.container}>
+//       <Text>{selectedMeal.title}</Text>
+//       <Button
+//         title='Go back to categories'
+//         onPress={() => {
+//           props.navigation.popToTop();
+//         }}
+//       />
+//     </View>
+//   );
+// };
+
+// MealDetailsScreen.navigationOptions = (navigationData) => {
+//   const mealId = navigationData.navigation.getParam('mealId');
+//   const selectedMeal = MEALS.find((meal) => {
+//     return meal.id === mealId;
+//   });
+//   return {
+//     headerTitle: selectedMeal.title,
+//     headerRight: () => {
+//       return (
+//         <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+//           <Item title='favorite' iconName='md-star' onPress={() => {}} />
+//         </HeaderButtons>
+//       );
+//     },
+//   };
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+// export default MealDetailsScreen;
